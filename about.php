d33f12cf052fbd3655e3b3daa09a55a97940287a
James Castrejon <jamescastrejon12@gmail.com>
2018-08-19 16:05:38 -0600

    Created css and default webpages for website.

---
+++ about.php
@@ -0,0 +1,31 @@
+<?php $location = "Home"; ?>
+<body><!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $location; ?></title>
+        <link href='styles/style.css', rel='stylesheet'>
+        <link rel="stylesheet" href="CSS/style.css" type="text/css">
+    </head>
+    <body>
+    <div class="container">
+        <div id="nav">
+            <h1 id="logo"><?php echo $location; ?></h1>
+            <a href="/class/Final-Assignment">Home</a>
+            <a href="/class/Final-Assignment/about">About</a>
+        </div>
+        <div id="body">
+            <div class="outer">
+                <div class="middle">
+                    <div class="inner">
+                        <h2>Insert About Text Here...</h2>
+                        <!-- TODO: sjngksngisngskfk -->
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div class="footer">
+                <footer>©Copyright</footer>
+            </div>
+        </div>
+    </body>
+</html>
